75fb842d024857f6509f52309973146715454141
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-07-10 22:03:56 +0545

    New dbContext
    in Utilities

---
+++ Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
