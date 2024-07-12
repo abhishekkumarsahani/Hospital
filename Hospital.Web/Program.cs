05673cecc7e290d0b7891a66f813147511a1f8ab
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-07-12 12:51:24 +0545

    Database Migration
    Updated some view pages and first data migration

--- Hospital.Web/Program.cs
+++ Hospital.Web/Program.cs
@@ -1,6 +1,12 @@
 using Hospital.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Hospital.Utilities;
+using Hospital.Repositories.Interfaces;
+using Hospital.Repositories.Implementation;
+using hospitals.Utilities;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +14,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IDbInitializer,DbInitializer>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender,EmailSender>();
+
+builder.Services.AddRazorPages();
 
 
 var app = builder.Build();
@@ -23,13 +36,24 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+DataSedding();
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{Area=Patient}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+void DataSedding()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.
+            GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
